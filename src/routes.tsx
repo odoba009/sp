5c95b0e2f46b06880f4187a3f3e61f939f0e2016
tokelee <abdullahomotoke@gmail.com>
2025-01-29 10:23:32 -0800

    fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/home'
 import Pay from './pages/unlock'
 
